bd629e65d93c8b48eb17ac4452957abac89d607f
Mark Judson <mark.judson@leasequery.com>
2024-07-09 04:26:21 -0500

    feature: login, register, and logout functionality

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using IdentityManger.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityManger.Data;
-public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext(options);
+public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext(options)
+{
+	public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+}
+
