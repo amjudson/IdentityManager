e39f63bbd3a3571870aa458c0348b18d1b6e6aaf
Mark Judson <mark.judson@leasequery.com>
2024-07-09 13:50:09 -0500

    feature: user roles management

---
+++ Models/ViewModels/RolesViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace IdentityManger.Models.ViewModels;
+public class RolesViewModel
+{
+	public RolesViewModel()
+	{
+		RolesList = [];
+	}
+
+	public ApplicationUser User { get; set; }
+	public List<RoleSelection> RolesList { get; set; }
+}
+
+public class RoleSelection
+{
+	public string RoleName { get; set; }
+	public bool IsSelected { get; set; }
+}
